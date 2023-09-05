1f91933845b36db6f766c331e49de8ec38499a84
Yunus <obidxojayevbotir@gmail.com>
2023-09-05 17:49:14 +0500

    #i18n

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{t("navigation1")}</h1>
+      <h1>{t("navigation2")}</h1>
+      <h1>{t("navigation3")}</h1>
+      <h1>{t("navigation4")}</h1>
+        <button onClick={() => changeLanguage("uz")}>uz</button>
+        <button onClick={() => changeLanguage("ru")}>ru</button>
+        <button onClick={() => changeLanguage("en")}>en</button>
     </div>
   );
 }
