4de18a8ab62eb2c9bf0cb83f7ee1f2778740a1af
Yunus <128239091+theyuunus@users.noreply.github.com>
2023-12-09 16:49:14 +0500

    #fix #fix and change codes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import { useTranslation } from "react-i18next";
 
 function App() {
@@ -7,15 +7,35 @@ function App() {
   const changeLanguage = (language) => {
     i18n.changeLanguage(language);
   };
+  console.clear();
   return (
-    <div className="App">
-      <h1>{t("navigation1")}</h1>
-      <h1>{t("navigation2")}</h1>
-      <h1>{t("navigation3")}</h1>
-      <h1>{t("navigation4")}</h1>
-        <button onClick={() => changeLanguage("uz")}>uz</button>
-        <button onClick={() => changeLanguage("ru")}>ru</button>
-        <button onClick={() => changeLanguage("en")}>en</button>
+    <div className="main">
+      <div className="main-div">
+        <h1>{t("navigation1")}</h1>
+        <h1>{t("navigation2")}</h1>
+        <h1>{t("navigation3")}</h1>
+        <h1>{t("navigation4")}</h1>
+      </div>
+      <div className="main-buttons">
+        <button
+          className="main-buttons-button"
+          onClick={() => changeLanguage("uz")}
+        >
+          uz
+        </button>
+        <button
+          className="main-buttons-button"
+          onClick={() => changeLanguage("ru")}
+        >
+          ru
+        </button>
+        <button
+          className="main-buttons-button"
+          onClick={() => changeLanguage("en")}
+        >
+          en
+        </button>
+      </div>
     </div>
   );
 }
